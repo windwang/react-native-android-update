b7f1b9bf33b143f95b30066450094be1c0c46e14
windwang <windwang@gmail.com>
2016-10-28 13:19:27 +0800

    提交更新代码

--- index.android.js
+++ index.android.js
@@ -1,15 +1,25 @@
 'use strict';
-
-var { NativeModules } = require('react-native');
-var NativeRNUpdate = NativeModules.RNUpdate
-
-class RNUpdate {
-  constructor() {
-  }
-
-  static update(url) {
-    NativeRNUpdate.update(url);
-  }
+import {NativeModules, AppState} from 'react-native'
+let NativeRNUpdate = NativeModules.RNUpdate
+let started = false;
+let checkUrl = null;
+function update(url) {
+  NativeRNUpdate.update(url);
 }
 
-module.exports = RNUpdate;
+function onAppStateChange(newState) {
+  newState === "active" && NativeRNUpdate.update(url)
+}
+
+function start(url) {
+  if (started) 
+    return;
+  started = true;
+  checkUrl = url;
+
+  AppState.addEventListener("change", onAppStateChange);
+}
+module.exports = {
+  start,
+  update
+}
