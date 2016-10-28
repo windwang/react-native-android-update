0aa7e47c5f5a14356e8153769d85907c67499f9c
windwang <windwang@gmail.com>
2016-10-28 16:03:26 +0800

    没有更新时不提示

--- android/src/main/java/com/smn/update/UpdateListener.java
+++ android/src/main/java/com/smn/update/UpdateListener.java
@@ -1,8 +1,14 @@
 package com.smn.update;
 
+import com.github.snowdream.android.app.DefaultUpdateListener;
+
 /**
  * Created by wwm on 2016-10-28.
  */
 
-public class UpdateListener {
+public class UpdateListener extends DefaultUpdateListener {
+    @Override
+    public void onShowNoUpdateUI() {
+
+    }
 }
